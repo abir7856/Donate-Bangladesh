b223320725eb377694108831e098c1e9869b9e62
abir7856 <abdullahhilbakyee@gmail.com>
2025-01-19 20:11:04 +0600

    Complete History Page

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function getInputValue(id) {
 //     mainBalance - noakhaliDonateUserAmount;
 // });
 
-function donationCalculator(DonationAmount, totalDonation) {
+function donationCalculator(DonationAmount, totalDonation, event) {
   const userDonation = parseFloat(
     document.getElementById(DonationAmount).value
   );
@@ -41,14 +41,51 @@ function donationCalculator(DonationAmount, totalDonation) {
 
   document.getElementById("main-balance").innerText =
     accountBalance - userDonation;
+
+  const eventName = document.getElementById(event).innerText;
+
+  const historyContainer = document.getElementById("history-container");
+  const div = `
+      <div class="p-4 md:p-6 lg:p-8 rounded-2xl border space-y-4 mt-4">
+          <h3 class="text-2xl font-bold">
+            ${userDonation} Taka is ${eventName}
+          </h3>
+          <p class="text-base text-gray-600">
+            Date: ${new Date().toString()}
+          </p>
+        </div>
+    `;
+  historyContainer.innerHTML += div;
+
+  document.getElementById(DonationAmount).value = "";
 }
 // const noakhali = donationCalculator("noakhali-user-donate-amount");
 document.getElementById("noakhali-donate-btn").addEventListener("click", () => {
-  donationCalculator("noakhali-user-donate-amount", "noakhali-total-Donation");
+  donationCalculator(
+    "noakhali-user-donate-amount",
+    "noakhali-total-Donation",
+    "noakhali-event"
+  );
 });
 document.getElementById("feni-donate-btn").addEventListener("click", () => {
-  donationCalculator("feni-user-donate-amount", "feni-total-donation");
+  donationCalculator(
+    "feni-user-donate-amount",
+    "feni-total-donation",
+    "feni-event"
+  );
 });
 document.getElementById("quata-protest-btn").addEventListener("click", () => {
-  donationCalculator("quata-user-donation", "quota-total-donation");
+  donationCalculator(
+    "quata-user-donation",
+    "quota-total-donation",
+    "quota-event"
+  );
+});
+
+document.getElementById("history-btn").addEventListener("click", () => {
+  document.getElementById("donation-container").classList.add("hidden");
+  document.getElementById("history-container").classList.remove("hidden");
+});
+document.getElementById("donate-btn").addEventListener("click", () => {
+  document.getElementById("donation-container").classList.remove("hidden");
 });
